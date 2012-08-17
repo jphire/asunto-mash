9ddb248823dc77d09fcc0abc4932707f9faf0d85
Janne Laukkanen <janne.laukkanen83@gmail.com>
2012-08-17 18:18:38 +0300

    add small fixes

--- Gemfile
+++ Gemfile
@@ -5,8 +5,9 @@ gem 'rails', '3.2.5'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
+gem 'capybara-webkit', :git => 'git://github.com/wooandoo/capybara-webkit.git'
 gem 'sqlite3'
-gem 'bootstrap-sass', '2.0.0'
+gem 'bootstrap-sass', '2.0.3'
 gem 'haml'
 gem 'thin'
 
--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -1,6 +1,67 @@
 # encoding: utf-8
-class MainsController < ApplicationController
+require "rubygems"
+require "bundler/setup"
+require "capybara"
+require "capybara/dsl"
+require "capybara-webkit"
+Capybara.run_server = false
+Capybara.current_driver = :webkit
+Capybara.app_host = "http://kuluttaja.etuovi.com/"
+Capybara.default_wait_time = 5
 
+class MainsController < ApplicationController
+  include Capybara::DSL
+  
   def home
+    @results = []
+    if params[:search_appartment]
+      @results = get_results params[:search_appartment]
+    else
+      flash[:notice] = params
+    end
   end
+  
+  private 
+    
+    def get_results(q)
+      temp_list1 = []
+      Capybara.app_host = "http://kuluttaja.etuovi.com/"
+      visit('/')
+      fill_in "fs_areafield", :with => q
+      click_button "hae_pikahaku"
+      wait_until { page.find('#container').visible? }
+     
+      all(:css, "table#searchList tr[style='background-color: #E9E9E9;']").each do |appartment| 
+        a_hash = appartment.base.inner_html
+        temp_list1.push a_hash
+      end
+      
+      temp_list2 = []
+      Capybara.app_host = "http://asunnot.oikotie.fi/"   
+      visit('/')
+      fill_in "magicbox", :with => q
+      click_button "index-move-to-search"
+      wait_until { page.find('#container').visible? }
+      
+      all(:css, "ul.cards li .content").each do |appartment|       
+        a_hash = appartment.base.inner_html
+        temp_list2.push a_hash
+      end
+      
+      temp_list3 = []
+      Capybara.app_host = "http://www.jokakoti.fi/etusivu"   
+      visit('/')
+      i = find(:css, '#searchbox .areapicker input')[0]
+      fill_in i, :with => q
+      button = find(:css, '#searchButtons :first-child :first-child')[0]
+      click_button button
+      wait_until { page.find('#maincontent').visible? }
+     
+      all(:css, "#searchResultsListPanel li").each do |appartment| 
+        a_hash = appartment.base.inner_html
+        temp_list3.push a_hash
+      end
+      
+      return temp_list1, temp_list2, temp_list3
+    end
 end
